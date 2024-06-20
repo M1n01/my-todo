65221ab851fe5000fad454bc83e04c9f8bf8abfd
M1n01 <abe.minato.bz@gmail.com>
2024-06-20 13:22:52 +0900

    feat: delete feature

--- todo-front/src/lib/api/todo.ts
+++ todo-front/src/lib/api/todo.ts
@@ -23,3 +23,28 @@ export const getTodoItems = async () => {
   const json: Todo[] = await res.json();
   return json;
 };
+
+export const updateTodoItem = async (todo: Todo) => {
+  const { id, ...updateTodo } = todo;
+  const res = await fetch(`http://localhost:3000/todos/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updateTodo),
+  });
+  if (!res.ok) {
+    throw new Error('Failed to update todo item');
+  }
+  const json: Todo = await res.json();
+  return json;
+};
+
+export const deleteTodoItem = async (id: number) => {
+  const res = await fetch(`http://localhost:3000/todos/${id}`, {
+    method: 'DELETE',
+  });
+  if (!res.ok) {
+    throw new Error('Failed to delete todo item');
+  }
+};
