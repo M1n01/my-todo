fad5e16358d5d5e715232497bcb5ddbfe396b303
M1n01 <abe.minato.bz@gmail.com>
2024-06-20 12:58:52 +0900

    update: todo list as api connection

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ use axum::{
     Router,
 };
 use handlers::{all_todo, create_todo, delete_todo, find_todo, update_todo};
-use std::net::SocketAddr;
-use std::{env, sync::Arc};
 
-use dotenv::dotenv;
+use hyper::header::CONTENT_TYPE;
 use sqlx::PgPool;
+use std::net::SocketAddr;
+use dotenv::dotenv;
+use std::{env, sync::Arc};
+use tower_http::cors::{Any, CorsLayer, Origin};
 
 #[tokio::main]
 async fn main() {
@@ -50,6 +52,12 @@ fn create_app<T: TodoRepository>(repository: T) -> Router {
                 .patch(update_todo::<T>),
         )
         .layer(Extension(Arc::new(repository)))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Origin::exact("http://localhost:3001".parse().unwrap()))
+                .allow_methods(Any)
+                .allow_headers(vec![CONTENT_TYPE]),
+        )
 }
 
 async fn root() -> &'static str {
